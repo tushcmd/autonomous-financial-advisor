f0fc13d59834b8a6f7906166adbde267dace59a5
tushcmd <muturidavid854@gmail.com>
2025-04-04 00:38:49 +0300

    refactor: paths config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compilerOptions: {
+    // other options
+    baseUrl: ".",
+    paths: {
+      "@/*": ["src/*"],
+    },
+  },
 };
 
 export default nextConfig;
