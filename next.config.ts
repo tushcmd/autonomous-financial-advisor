eba60689e2b719e97496956a41340d1e9a80fbf5
tushcmd <muturidavid854@gmail.com>
2025-05-01 11:32:42 +0300

    config for mastra

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // ✅ Keep any other valid config options here (not compilerOptions!)
+  serverExternalPackages: ["@mastra/*"],
 };
 
 export default nextConfig;
